0db06558b3088115462899ad71a89e2826c9d4ab
yait-nas <yait-nas@c3r3p9.1337.ma>
2026-01-04 17:10:10 +0100

    add: read from csv file

--- quiz.go
+++ quiz.go
@@ -5,7 +5,14 @@ import	(
 	"fmt"
 	"os"
 	"flag"
+	"encoding/csv"
+	"io"
+	"log"
+	"strconv"
 )
+
+func	displayScore() {
+}
 
 func	main() {
 	var	fNameErrorMsg	string	= "a csv file in the format of 'question,answer'";
@@ -16,10 +23,30 @@ func	main() {
 	flag.Parse();
 	fmt.Println("file name:", *fileNamePtr);
 	fmt.Println("limit:", *limitPtr);
-	file, err	:= os.Open(*fileNamePtr);
+	csvFile, err	:= os.Open(*fileNamePtr);
 	if err != nil {
 		fmt.Println("Error opening file:", err);
 		return;
 	}
-	defer	file.Close();
+	defer	csvFile.Close();
+	reader	:= csv.NewReader(csvFile);
+	index	:= 1;
+	for {
+		lineTokens, err := reader.Read();
+		if err == io.EOF {
+			displayScore();
+			return ;
+		} else if err != nil {
+			log.Fatal(err);
+			// return ;
+		}
+		question	:= lineTokens[0];
+		answerS		:= lineTokens[1];
+		answerI, err	:= strconv.Atoi(answerS);
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Problem #%d:\t%s\t= %d\n", index, question, answerI);
+		index++;
+	}
 }
